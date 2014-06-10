a101a9c4139cc09e2cfd2c5a1d645243ced05bf0
Wouter van der Meulen Kunee <w.vandermeulenkunee@finan.nl>
2014-06-10 15:44:24 +0200

    Add select multiple stories

--- src/main/webapp/scripts/controllers/main.js
+++ src/main/webapp/scripts/controllers/main.js
@@ -1,29 +1,73 @@
 'use strict';
-angular.module('jbehave').controller('StoryCtrl', ['$scope','$http', function StoryCtrl($scope,$http) {
+angular.module('jbehave').controller('StoryCtrl', ['$scope','$http', 'filterFilter', function StoryCtrl($scope,$http,filterFilter) {
   $scope.stories = [];
+  $scope.checkedStories = [];
+  $scope.scenarios = [];
 
   $scope.loadStories = function() {    
-  /**  $http({method: 'GET', url: 'http://www.mocky.io/v2/5396c57bb85ce45918d73ac6'}).**/
-  /** $http({method: 'GET', url: 'http://erwin-finan.finan.local:8080/runner/stories'}).
+   $http({method: 'GET', url: '/runner/api/stories'}).
     success(function(data,status,header,config){
-    var storiesResult = JSON.parse(data);
+      console.log(data);
+    var storiesResult = data;
         angular.forEach(storiesResult, function(story) {
           console.log('story = ' + story);
-         $scope.stories.push({text:story, done:false});
+         $scope.stories.push({text:story.name});
         });
-    }); **/
-  var data = '[{"name":"scenario description","steps":["Given a system state","When I do something","Then system is in a different state"]},{"name":"scenario description 2","steps":["Given a system state","When I do something","Then system is in a different state"]}]';
-
-  var storiesResult = JSON.parse(data);
-        angular.forEach(storiesResult, function(story) {
-          console.log('story = ' + story.name);
-          if (!story.done) $scope.stories.push({text:story.name});
-        });
+    }); 
   };
 
+  $scope.loadScenarios = function($story) {    
+   $http({method: 'GET', url: '/runner/api/stories/' + $story + '/scenarios'}).
+    success(function(data,status,header,config){
+      console.log(data);
+    var scenariosResult = data;
+        angular.forEach(scenariosResult, function(scenario) {
+          console.log('scenario = ' + scenario);
+         $scope.scenarios.push({text:scenario.name});
+        });
+    }); 
+  };
+ 
   $scope.submitStories = function() {
     // submit data
-    console.log('Submitting scenarios ' + $scope.story.done);
+    console.log('Submitting scenarios ' + $scope.checkedStories);
   };
-
+  
+  $scope.selectStory = function(story) {
+  
+    if ($scope.checkedStories.indexOf(story) != -1) return;
+    $scope.checkedStories.push(story);
+    console.log('Submitting story ' + story);
+  }
 }]);
+
+angular.module('jbehave').directive('checkList', function() {
+  return {
+    scope: {
+      list: '=checkList',
+      value: '@'
+    },
+    link: function(scope, elem, attrs) {
+      var handler = function(setup) {
+        var checked = elem.prop('checked');
+        var index = scope.list.indexOf(scope.value);
+
+        if (checked && index == -1) {
+          if (setup) elem.prop('checked', false);
+          else scope.list.push(scope.value);
+        } else if (!checked && index != -1) {
+          if (setup) elem.prop('checked', true);
+          else scope.list.splice(index, 1);
+        }
+      };
+      
+      var setupHandler = handler.bind(null, true);
+      var changeHandler = handler.bind(null, false);
+            
+      elem.on('change', function() {
+        scope.$apply(changeHandler);
+      });
+      scope.$watch('list', setupHandler, true);
+    }
+   };
+});
