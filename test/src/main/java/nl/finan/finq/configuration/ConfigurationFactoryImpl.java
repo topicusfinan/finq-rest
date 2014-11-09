d5156ef38595660566d625e5d6810abb31f41836
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-11-09 17:38:29 +0100

    fix compile error

--- test/src/main/java/nl/finan/finq/configuration/ConfigurationFactoryImpl.java
+++ test/src/main/java/nl/finan/finq/configuration/ConfigurationFactoryImpl.java
@@ -1,11 +1,8 @@
 package nl.finan.finq.configuration;
 
 import nl.eernie.jmoribus.configuration.Configuration;
-import nl.eernie.jmoribus.configuration.DefaultConfiguration;
 import nl.eernie.jmoribus.reporter.Reporter;
 import nl.finan.finq.embeder.ConfigurationFactory;
-import nl.finan.finq.embeder.WebStoryReporter;
-import nl.finan.finq.factory.BeanFactory;
 import nl.finan.finq.steps.Step;
 
 import javax.ejb.EJB;
