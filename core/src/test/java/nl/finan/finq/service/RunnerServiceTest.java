8a9c34e9b58e819596c6f2eb2e86b35c30166d9d
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2014-11-09 17:03:57 +0100

    Fix all tests

--- core/src/test/java/nl/finan/finq/service/RunnerServiceTest.java
+++ core/src/test/java/nl/finan/finq/service/RunnerServiceTest.java
@@ -17,6 +17,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.jms.*;
 
 import static org.mockito.Matchers.any;
 import static org.powermock.api.mockito.PowerMockito.mock;
@@ -35,36 +36,50 @@ public class RunnerServiceTest {
     @Mock
     private ManagedExecutorService managedExecutorService;
 
+    @Mock
+    private Queue queue;
+
+    @Mock
+    private ConnectionFactory connectionFactory;
+
     @InjectMocks
     private RunnerService runnerService;
 
-    private StoryRunner storyRunner = mock(StoryRunner.class);
+    private ObjectMessage message;
+    private MessageProducer producer;
 
     @Before
-    public void before(){
-        PowerMockito.mockStatic(BeanFactory.class);
-        when(BeanFactory.getBean("java:module/StatefulStoryRunner")).thenReturn(storyRunner);
-
+    public void before() throws JMSException {
+        message = mock(ObjectMessage.class);
+        Connection connection = mock(Connection.class);
+        Session session = mock(Session.class);
+        producer = mock(MessageProducer.class);
+        when(connectionFactory.createConnection()).thenReturn(connection);
+        when(connection.createSession(false, Session.AUTO_ACKNOWLEDGE)).thenReturn(session);
+        when(session.createProducer(queue)).thenReturn(producer);
+        when(session.createObjectMessage()).thenReturn(message);
     }
 
     @Test
-    public void runStory(){
+    public void runStory() throws JMSException {
         Story story = mock(Story.class);
         RunningStories run = runnerService.run(story);
 
         Mockito.verify(runningStoriesDao).persist(run);
         Assert.assertEquals(LogStatus.RUNNING, run.getStatus());
-        Mockito.verify(managedExecutorService).execute(storyRunner);
+
+        Mockito.verify(producer).send(message);
     }
 
     @Test
-    public void runBundle(){
+    public void runBundle() throws JMSException {
         Book book = mock(Book.class);
         RunningStories run = runnerService.run(book);
 
         Mockito.verify(runningStoriesDao).persist(run);
         Assert.assertEquals(LogStatus.RUNNING, run.getStatus());
-        Mockito.verify(managedExecutorService).execute(storyRunner);
+
+        Mockito.verify(producer).send(message);
     }
 
 }
